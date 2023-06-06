64579c5d59820c8380458bed1a59531262b02d55
Pritam-01 <98100177+Pritam-01@users.noreply.github.com>
2023-06-06 20:05:33 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
+
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+      <div className="box" id="box-1">navbar</div>
+      <div className="box" id="box-2">Cricket</div>
+      <div className="box" id="box-3">Coaches</div>
+      <div className="box" id="box-4">Coordinator</div>
+      <div className="box" id="box-5">Players</div>
+   </div>
   );
 }
 
